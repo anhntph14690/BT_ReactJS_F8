5ab7594908ae19e3da49e6bea14495685543cdaa
anhntph14690 <anhntph14690@fpt.edu.vn>
2022-09-26 13:34:11 +0700

    update 2.7

--- my-app/src/page/2.7/index.tsx
+++ my-app/src/page/2.7/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 
-type Props = {}
 
-const Bai2_7 = (props: Props) => {
+// type Props ={
 
+// }
+type Type = {
+    [index: string]: number|string
+}
 
-    function arrToObj(arr: any) {
-        return arr.reduce((innit: any, value: any) => {
-          innit[value[0]] = value[1]
-          return innit
-       }, {}) 
-    }
+const Bai2_7 = (props: Type) => {
+    // function arrToObj(arr: any) {
+    //     return arr.reduce((innit: any, value: any) => {
+    //       innit[value[0]] = value[1]
+    //       return innit
+    //    }, {}) 
+    // }
+
+    const infoArr:( (number|string)[] )[] = [
+        ['name', 'The Anh'],
+        ['age', 20],
+        ['address', 'Ha Noi']
+    ];
+
+    const arrToObj = (arr:( (number|string)[])[]) => arr.reduce((acc:Type, cur:(number|string)[]) :Type=> {
+        console.log(acc);
+        return {...acc, [cur[0]]:cur[1]}
+    }, {});
+
+    console.log(arrToObj(infoArr));
     
    
 
